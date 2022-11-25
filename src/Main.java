7846932f371ed59a9b714cf359371811656b4746
Jappananny <Jappananny@gmail.com>
2022-11-26 00:51:43 +0300

    ДЗ 2.8/1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ProductException {
+
+        Product bamamas = new Product("Банан",89.90,0.5);
+        System.out.println(bamamas);
+        Product nail = new Product("Деревянные гвозди", 820,3.0);
+        System.out.println(nail);
+        Product rice = new Product("Рис",119.99,10.5);
+        System.out.println(rice);
+        Product sand = new Product("Песок строительный",6, 4500.0);
+        System.out.println(sand);
+        ProductS productS = new ProductS();
+        productS.add(bamamas);
+        productS.add(nail);
+        System.out.println("======================================================");
+        productS.toString();
+        productS.remove(bamamas);
+        productS.toString();
+
+
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,44 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+
+@Getter
+@Setter
+public class Product {
+    private String name;
+    private double priceForKillo;
+    private double killo;
+    private boolean buy;
+
+
+    public Product(String name,double priceForKillo, double killo) throws ProductException {
+        if (name.length() <= 0 || name == null)
+            throw new ProductException("Что за продукт");
+        else {
+            this.name = name;
+        }
+        this.priceForKillo = priceForKillo * killo;
+        this.killo = killo;
+    }
+    public void buy() {
+        buy = true;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+    @Override
+    public String toString() {
+        return "Продукт " + this.name + ", стоит " + this.priceForKillo + " руб. и весит "
+                + this.killo + " кг.";
+    }
+}
