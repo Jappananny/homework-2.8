6d29051142472bad9aa606614c0081ab49130986
Jappananny <Jappananny@gmail.com>
2022-11-27 02:30:16 +0300

    ДЗ 2.8/3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
+    private static Random RANDOM = new Random();
     public static void main(String[] args) throws ProductException {
         Product bananas = new Product("Банан",89.90,0.5);
         System.out.println(bananas);
@@ -20,6 +25,23 @@ public class Main {
         trade.add(fireNeil);
         System.out.println(fireNeil);
         System.out.println("======================================================");
+        //Задача 3
+        Set<Integer> number = new HashSet<>();
+        Set<Integer> numberRemuve = new HashSet<>();
+        for (int i=1; i <=20;i++){
+            number.add(RANDOM.nextInt(1001));
+        }
+        for (Integer number1: number) {
+            if (!(number1 % 2 == 0)){
+                numberRemuve.add(number1);
+            } else {
+                System.out.print(number1+" ");
+            }
+        }
+        for (Integer number1: numberRemuve) {
+            number.remove(number1);
+        }
+
 
 
 
